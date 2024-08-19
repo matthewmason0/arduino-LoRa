d6962f670ca5e770b8efd7312de5642a4fda1f12
Matt Mason <matthewmason0@gmail.com>
2024-08-18 20:50:37 -0500

    Change indentation to 4 spaces

--- src/LoRa.h
+++ src/LoRa.h
@@ -18,100 +18,100 @@
 
 class LoRaClass : public Stream {
 public:
-  LoRaClass();
+    LoRaClass();
 
-  bool begin(long frequency);
-  void end();
+    bool begin(long frequency);
+    void end();
 
-  bool beginPacket(int implicitHeader = false);
-  bool endPacket(bool async = false);
+    bool beginPacket(int implicitHeader = false);
+    bool endPacket(bool async = false);
 
-  uint8_t parsePacket(uint8_t size = 0);
-  int packetRssi();
-  float packetSnr();
-  long packetFrequencyError();
+    uint8_t parsePacket(uint8_t size = 0);
+    int packetRssi();
+    float packetSnr();
+    long packetFrequencyError();
 
-  int rssi();
+    int rssi();
 
-  // from Print
-  virtual size_t write(uint8_t byte);
-  virtual size_t write(const uint8_t *buffer, size_t size);
+    // from Print
+    virtual size_t write(uint8_t byte);
+    virtual size_t write(const uint8_t *buffer, size_t size);
 
-  // from Stream
-  virtual int available();
-  virtual int read();
-  virtual int peek();
-  virtual void flush();
+    // from Stream
+    virtual int available();
+    virtual int read();
+    virtual int peek();
+    virtual void flush();
 
-  void onReceive(void(*callback)(uint8_t));
-  void onTxDone(void(*callback)());
+    void onReceive(void(*callback)(uint8_t));
+    void onTxDone(void(*callback)());
 
-  void continuousRx(uint8_t size = 0);
-  void singleRx();
-  void idle();
-  void sleep();
+    void continuousRx(uint8_t size = 0);
+    void singleRx();
+    void idle();
+    void sleep();
 
-  void setTxPower(uint8_t level, uint8_t outputPin = PA_OUTPUT_PA_BOOST_PIN);
-  void setFrequency(long frequency);
-  void setSpreadingFactor(uint8_t sf);
-  void setSignalBandwidth(long sbw);
-  void setCodingRate4(uint8_t denominator);
-  void setPreambleLength(uint16_t length);
-  void setSyncWord(uint8_t sw);
-  void enableCrc();
-  void disableCrc();
-  void enableInvertIQ();
-  void disableInvertIQ();
-  
-  void setOCP(uint8_t mA); // Over Current Protection control
-  
-  void setGain(uint8_t gain); // Set LNA gain
+    void setTxPower(uint8_t level, uint8_t outputPin = PA_OUTPUT_PA_BOOST_PIN);
+    void setFrequency(long frequency);
+    void setSpreadingFactor(uint8_t sf);
+    void setSignalBandwidth(long sbw);
+    void setCodingRate4(uint8_t denominator);
+    void setPreambleLength(uint16_t length);
+    void setSyncWord(uint8_t sw);
+    void enableCrc();
+    void disableCrc();
+    void enableInvertIQ();
+    void disableInvertIQ();
 
-  // deprecated
-  void crc() { enableCrc(); }
-  void noCrc() { disableCrc(); }
+    void setOCP(uint8_t mA); // Over Current Protection control
 
-  uint8_t random();
+    void setGain(uint8_t gain); // Set LNA gain
 
-  void setPins(uint8_t ss = LORA_DEFAULT_SS_PIN, uint8_t reset = LORA_DEFAULT_RESET_PIN, uint8_t dio0 = LORA_DEFAULT_DIO0_PIN);
-  void setSPI(SPIClass& spi);
-  void setSPIFrequency(uint32_t frequency);
+    // deprecated
+    void crc() { enableCrc(); }
+    void noCrc() { disableCrc(); }
 
-  void dumpRegisters(Stream& out);
-  void debug();
+    uint8_t random();
 
-private:
-  void explicitHeaderMode();
-  void implicitHeaderMode();
+    void setPins(uint8_t ss = LORA_DEFAULT_SS_PIN, uint8_t reset = LORA_DEFAULT_RESET_PIN, uint8_t dio0 = LORA_DEFAULT_DIO0_PIN);
+    void setSPI(SPIClass& spi);
+    void setSPIFrequency(uint32_t frequency);
 
-  void handleDio0Rise();
-  // bool isTransmitting();
-  void resetFifo();
-
-  uint8_t getSpreadingFactor();
-  long getSignalBandwidth();
-
-  void setLdoFlag();
-
-  uint8_t readRegister(uint8_t address);
-  void writeRegister(uint8_t address, uint8_t value);
-  uint8_t singleTransfer(uint8_t address, uint8_t value);
-
-  static void onDio0Rise();
+    void dumpRegisters(Stream& out);
+    void debug();
 
 private:
-  SPISettings _spiSettings;
-  SPIClass* _spi;
-  uint8_t _ss;
-  uint8_t _reset;
-  uint8_t _dio0;
-  long _frequency;
-  bool _implicitHeaderMode;
-  volatile bool _transmitting;
-  volatile uint8_t _packetIndex;
-  volatile uint8_t _available;
-  void (*_onReceive)(uint8_t);
-  void (*_onTxDone)();
+    void explicitHeaderMode();
+    void implicitHeaderMode();
+
+    void handleDio0Rise();
+    // bool isTransmitting();
+    void resetFifo();
+
+    uint8_t getSpreadingFactor();
+    long getSignalBandwidth();
+
+    void setLdoFlag();
+
+    uint8_t readRegister(uint8_t address);
+    void writeRegister(uint8_t address, uint8_t value);
+    uint8_t singleTransfer(uint8_t address, uint8_t value);
+
+    static void onDio0Rise();
+
+private:
+    SPISettings _spiSettings;
+    SPIClass* _spi;
+    uint8_t _ss;
+    uint8_t _reset;
+    uint8_t _dio0;
+    long _frequency;
+    bool _implicitHeaderMode;
+    volatile bool _transmitting;
+    volatile uint8_t _packetIndex;
+    volatile uint8_t _available;
+    void (*_onReceive)(uint8_t);
+    void (*_onTxDone)();
 };
 
 extern LoRaClass LoRa;
